e53e06918d3e4fa18c286ce3e2c8a16f880f3ea8
ramtarun02 <ta.ramtarun.19@gmail.com>
2026-04-01 22:52:04 +0530

    project structure

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+
+struct caseParameters {
+  int numPoints;
+  double xLength;
+  double yLength;
+  double CFL;
+  double gamma;
+  double dx;
+  double dy;
+  double endTime;
+  int timeStep;
+};
+
+void startSimulation(struct caseParameters, double initialConditions,
+                     int scheme, int limiter) {
+  return
+};
+
+int main() {
+ caseParameters case;
+
+ startSimulation(struct caseParameters, double initialConditions, int scheme, int limiter);
+
+ 
+  
+}
